78cfd408cd2786a0a581226cdd01e872465a3583
Raghu Betina <raghu@firstdraft.com>
2020-07-22 12:57:42 -0500

    Stub routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get("/", { :controller => "misc", :action => "homepage" })
+  
+  get("/forex", { :controller => "currencies", :action => "index" })
+  get("/forex/:from_currency/:to_currency", { :controller => "currencies", :action => "convert" })
+  
+  get("/covid", { :controller => "pandemic", :action => "index" })
+  get("/covid/:state", { :controller => "pandemic", :action => "details" })
 end
